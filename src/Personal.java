e3bac4f1397ead90f73e6dde2a2382572632745a
Jatinpatel <shivam1819patel@gmail.com>
2023-03-12 17:49:10 +0000

    Personal details

---
+++ src/Personal.java
@@ -0,0 +1,35 @@
+import java.nio.channels.AsynchronousByteChannel;
+import java.security.KeyStore;
+
+public class Personal {
+    // User defined method
+    static String name= "Adam";
+    static int age= 25;
+    static int MobileNumber =1234567891;
+    static String address= "38A,ABC Road,ABC AB1 2AB";
+
+    static int RollNumber = 17;
+    static char grade ='A';
+    static boolean eligibility = true ;
+    static double amount = 0.75;
+//Main method
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(MobileNumber);
+        System.out.println(address);
+        System.out.println(RollNumber);
+        System.out.println(grade);
+        System.out.println(eligibility);
+        System.out.println(amount);
+    }
+}
+
+
+
+
+
+
+
+
+
